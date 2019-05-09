1bc2e07f22adfec07f59ab816e54e5b0e61484e0
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-05-09 20:20:35 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |styles, info|
+    info.each do |lang, details|
+      #details.each do |key, value|
+        new_hash[lang] = details
+        new_hash[lang][:style] = [styles]
+        if lang == :javascript 
+          new_hash[lang][:style] = [:oo, :functional]
+        end
+      #end
+    end
+  end
+new_hash
 end
