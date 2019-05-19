d447186dc279266347d0e24212fa6aa95d1544ed
Alexander Schelchere <ascellie@gmail.com>
2019-05-19 09:05:28 -0400

    I see where the attributes are.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+    # create an empty object, reformed_languages
+    reformed_languages = {}
+    # for each style of language
+    languages.each do |style, bundle|
+
+      bundle.each do |language, language_type| 
+        puts "style: #{style} | lang: #{language} | type: #{language_type}"
+        
+      end
+
+  end
+
 end
