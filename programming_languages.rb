e0d3f7fb97c446b267c3e8eacb5635151fd456d7
Garrett Halstein <garrett.halstein@emory.edu>
2019-05-20 01:25:38 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |type, language|
+  	language.each do |lang, description|
+  	  if !(new_hash.include?(lang))
+  	    array = []
+  	    array.push(type)
+  	    new_hash[lang] = {type: description[:type], style: array}
+  	  else
+        new_hash[lang][:style].push(type)
+      end
+    end
+  end
+  new_hash
 end
