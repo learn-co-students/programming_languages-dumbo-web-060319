6f86a0c004ebca9b149b141f72bf7af69ba44887
github username <github email address>
2019-05-25 22:53:34 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,36 @@
+
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+
+
+
+#       {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+    # :style => [:oo, :functional]
+
 def reformat_languages(languages)
-  # your code here
+  result={}
+  arr=[]
+languages.each do |k1,v1|   #k1=:oo #v1={:ruby => { :type => "interpreted"}}
+  v1.each do |k2,v2|  #k2=ruby  v2={:type => "interpreted"}
+    v2.each do |k3,v3|  #k3=:type v3="interpreted"
+      arr=[k1]
+      if result[k2] != nil
+        arr= arr+ result[k2][:style]
+      end
+      result[k2]={k3=>v3, :style=>arr}
+    end
+  end
+end
+result
 end
