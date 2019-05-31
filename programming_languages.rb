e4b72f3ad756f885d13573d71990d205f404bff1
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-05-31 15:19:23 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,27 @@
-def reformat_languages(languages)
-  # your code here
+
+def reformat_languages (languages_by_style)
+  # your code here 
+  new_hash = {}
+
+  languages_by_style.each do |key, data|
+
+    data.each do |nested_key, nested_data|
+
+      #find out if key has been added
+        if new_hash.has_key? (nested_key)
+          #if it has been added, insert into array using <<
+          new_hash[nested_key][:style] << key
+          
+        #else do this:
+        else
+          new_hash[nested_key] = nested_data
+          new_hash[nested_key][:style] = [key]
+        end
+        
+    end
+  end
+    new_hash
+    #binding.pry
 end
+
+#reformat_languages
